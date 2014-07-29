c8bd5c44ed59bc5a8aa38971000f042ba8989e94
netonjm <jmedrano@developstudios.com>
2014-07-29 12:14:15 +0200

    Fixed airhockey

--- AirHockey/src/win32/Program.cs
+++ AirHockey/src/win32/Program.cs
@@ -5,20 +5,20 @@ using CocosSharp;
 namespace AirHockey.Windows
 {
 
-    static class Program
-    {
+	static class Program
+	{
 
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        static void Main(string[] args)
-        {
-            CCApplication sharedApp = CCApplication.SharedApplication;
-            sharedApp.ApplicationDelegate = new AppDelegate();
-            CCApplication.SharedApplication.StartGame();
-        }
-    }
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			CCApplication application = new CCApplication(false, new CCSize(1024f, 768f));
+			application.ApplicationDelegate = new AppDelegate();
+			application.StartGame();
+		}
+	}
 
 
 }
