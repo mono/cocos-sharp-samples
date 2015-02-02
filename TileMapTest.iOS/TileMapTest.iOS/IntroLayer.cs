6f6ce483382436428da6b5a8843ac87eb0ecce3a
Kenneth Pouncey <kjpou@pt.lu>
2015-02-02 19:10:12 +0100

    Add TileMapTest.iOS sample project

---
+++ TileMapTest.iOS/TileMapTest.iOS/IntroLayer.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using CocosSharp;
+using Microsoft.Xna.Framework;
+
+namespace TileMapTest.iOS
+{
+    public class IntroLayer : CCLayerColor
+    {
+        CCLabelTtf label;
+
+        public IntroLayer()
+        {
+
+            // create and initialize a Label
+            label = new CCLabelTtf("Hello CocosSharp", "MarkerFelt", 22);
+            label.AnchorPoint = CCPoint.AnchorMiddle;
+
+            // add the label as a child to this Layer
+            AddChild(label);
+
+            // setup our color for the background
+            Color = new CCColor3B(CCColor4B.Blue);
+            Opacity = 255;
+
+        }
+
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+
+            var tilemap = new CCTileMap("tilemaps/iso-test-zorder.tmx");
+            AddChild(tilemap);
+
+            // Use the bounds to layout the positioning of our drawable assets
+            var bounds = VisibleBoundsWorldspace;
+
+            // position the label on the center of the screen
+            label.Position = bounds.Center;
+
+            // Register for touch events
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesEnded = OnTouchesEnded;
+            AddEventListener(touchListener, this);
+        }
+
+        void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            if (touches.Count > 0)
+            {
+                // Perform touch handling here
+            }
+        }
+    }
+}
+
