760e935f6133875f527bf7b97ab1e824c673ce9e
Kenneth Pouncey <kjpou@pt.lu>
2015-02-02 17:07:11 +0100

    Add new TileMapTest project to accompany the Tilemap Pipeline post.

---
+++ TileMapTest/TileMapTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TileMapTest.Resources;
+
+namespace TileMapTest
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
