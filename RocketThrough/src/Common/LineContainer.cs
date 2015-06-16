62f3e43cf5c9d1c2cbb9111243dc6a011f1cff03
Jose Medrano <jose.medrano@xamarin.com>
2015-06-16 13:32:43 +0200

    Fixed RocketThought example to work with CCDrawNode

--- RocketThrough/src/Common/LineContainer.cs
+++ RocketThrough/src/Common/LineContainer.cs
@@ -5,7 +5,6 @@ using CocosSharp;
 
 namespace RocketThrought.Common
 {
-
 	public enum lineTypes
 	{
 		LINE_TEMP = 1,
@@ -31,12 +30,12 @@ namespace RocketThrought.Common
 		public int _dashSpace;
 		public CCSize _screenSize;
 
+        CCDrawNode drawNode;
+
 		public LineContainer()
 		{
-
-
+            drawNode = new CCDrawNode();
 		}
-
 
 		protected override void AddedToScene()
 		{
@@ -49,9 +48,7 @@ namespace RocketThrought.Common
 
 			_energyLineX = _screenSize.Width * 0.96f;
 			_energyHeight = _screenSize.Height * 0.8f;
-
 			//glLineWidth(8.0 * CC_CONTENT_SCALE_FACTOR());
-
 			reset();
 		}
 
@@ -64,10 +61,9 @@ namespace RocketThrought.Common
 		public override void Update(float dt)
 		{
 			base.Update(dt);
-
 			_energy -= dt * _energyDecrement;
 			if (_energy < 0) _energy = 0;
-
+            Draw ();
 		}
 
 		public void setEnergyDecrement(float value)
@@ -76,76 +72,54 @@ namespace RocketThrought.Common
 			//if (_energyDecrement > 0.07) _energyDecrement = 0.07;
 		}
 
-		protected override void Draw()
-		{
-			base.Draw();
+        protected void Draw()
+        {
+            CCColor4B color = new CCColor4B(1.0f, 1.0f, 1.0f, 1.0f);
+            
 
-			CCColor4B color = new CCColor4B(1.0f, 1.0f, 1.0f, 1.0f);
+            switch (_lineType)
+            {
+                case lineTypes.LINE_NONE:
+                    break;
+                case lineTypes.LINE_TEMP:
+                    drawNode.DrawLine(_tip, _pivot, color);
+                    drawNode.DrawCircle(_pivot, 10, 10, color);
+                    break;
 
-			CCDrawingPrimitives.Begin();
+                case lineTypes.LINE_DASHED:
+                    drawNode.DrawCircle(_pivot, 10, 10, color);
+                    int segments = (int)(_lineLength / (_dash + _dashSpace));
+                    float t = 0.0f;
+                    float x_;
+                    float y_;
 
-			switch (_lineType)
-			{
-				case lineTypes.LINE_NONE:
-					break;
-				case lineTypes.LINE_TEMP:
+                    for (int i = 0; i < segments + 1; i++)
+                    {
+                        x_ = _pivot.X + t * (_tip.X - _pivot.X);
+                        y_ = _pivot.Y + t * (_tip.Y - _pivot.Y);
+                        drawNode.DrawCircle(new CCPoint(x_, y_), 4, 6, color);
+                        t += (float)1 / segments;
+                    }
+                    break;
+            }
 
-					//CCDrawingPrimitives.Begin();
-					CCDrawingPrimitives.DrawLine(_tip, _pivot, color);
-					CCDrawingPrimitives.DrawCircle(_pivot, 10, CCMathHelper.ToRadians(360), 10, false, color);
-					//CCDrawingPrimitives.End();
+            //draw energy bar
+            color = new CCColor4B(0.0f, 0.0f, 0.0f, 1.0f);
 
-					break;
-
-				case lineTypes.LINE_DASHED:
-
-					//CCDrawingPrimitives.Begin();
-					CCDrawingPrimitives.DrawCircle(_pivot, 10, (float)Math.PI, 10, false, color);
-					//CCDrawingPrimitives.End();
-
-					int segments = (int)(_lineLength / (_dash + _dashSpace));
-
-					float t = 0.0f;
-					float x_;
-					float y_;
-
-					for (int i = 0; i < segments + 1; i++)
-					{
-
-						x_ = _pivot.X + t * (_tip.X - _pivot.X);
-						y_ = _pivot.Y + t * (_tip.Y - _pivot.Y);
-
-						//CCDrawingPrimitives.Begin();
-						CCDrawingPrimitives.DrawCircle(new CCPoint(x_, y_), 4, (float)Math.PI, 6, false, color);
-						//CCDrawingPrimitives.End();
-
-						t += (float)1 / segments;
-					}
-					break;
-			}
-
-			//draw energy bar
-			color = new CCColor4B(0.0f, 0.0f, 0.0f, 1.0f);
-
-			CCDrawingPrimitives.DrawLine(
-				new CCPoint(_energyLineX, _screenSize.Height * 0.1f),
-				new CCPoint(_energyLineX, _screenSize.Height * 0.9f),
-				color);
-
-			color = new CCColor4B(1.0f, 0.5f, 0.0f, 1.0f);
-
-			CCDrawingPrimitives.DrawLine(
-				new CCPoint(_energyLineX, _screenSize.Height * 0.1f),
-				new CCPoint(_energyLineX, _screenSize.Height * 0.1f + _energy * _energyHeight),
-				color);
-
-			CCDrawingPrimitives.End();
-		}
+            drawNode.DrawLine(
+                new CCPoint(_energyLineX, _screenSize.Height * 0.1f),
+                new CCPoint(_energyLineX, _screenSize.Height * 0.9f),
+                color);
+            color = new CCColor4B(1.0f, 0.5f, 0.0f, 1.0f);
+            drawNode.DrawLine(
+                new CCPoint(_energyLineX, _screenSize.Height * 0.1f),
+                new CCPoint(_energyLineX, _screenSize.Height * 0.1f + _energy * _energyHeight),
+                color);
+        }
 
 		public static LineContainer Create()
 		{
 			return new LineContainer();
 		}
 	}
-
 }
